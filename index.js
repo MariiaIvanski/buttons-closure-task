81cadae1d4b112578e6e00e8bdbb5d8f6bf32287
mariia-ivanski <mariia.ivanski@gmail.com>
2021-12-06 17:45:38 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-function changeColor() {
-  const buttons = document.querySelectorAll("button").forEach((btn) => {
-    btn.addEventListener("click", backgrColor);
-  });
-  function backgrColor(event) {
-    event.preventDefault();
-    let color = buttons.getAttribute("id");
-    color = "{$color}";
-    console.log(color);
-    // document.body.style.background = color;
-  }
+const buttons = document.querySelectorAll("button");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", changeColor);
+});
+
+function changeColor(event) {
+  event.preventDefault();
+  console.log(event.target.id);
+  let color = event.target.id;
+  backgrColor(color);
 }
+
+function backgrColor(color) {
+  document.body.style.background = color;
+}
+
+//function backgrColor(event) {
+//  event.preventDefault;
+
+//console.log(btn.getAttribute("id"));
+// var color = id;
+//function backgrColor() {
+// console.log(color);
