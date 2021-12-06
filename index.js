9857823dd962fd39a925d04701a5e3f1a6744879
mariia-ivanski <mariia.ivanski@gmail.com>
2021-12-06 18:04:13 +0200

    update index.js

--- index.js
+++ index.js
@@ -5,19 +5,10 @@ buttons.forEach((btn) => {
 
 function changeColor(event) {
   event.preventDefault();
-  console.log(event.target.id);
   let color = event.target.id;
   backgrColor(color);
+
+  function backgrColor(color) {
+    document.body.style.background = color;
+  }
 }
-
-function backgrColor(color) {
-  document.body.style.background = color;
-}
-
-//function backgrColor(event) {
-//  event.preventDefault;
-
-//console.log(btn.getAttribute("id"));
-// var color = id;
-//function backgrColor() {
-// console.log(color);
